16ecf867cdbfb21585228815bf63cc9711015a90
Parinit Mishra <parinitmishragate6@users.noreply.github.com>
2016-10-14 15:30:14 +0530

    Update TestDataProviderTests.java

--- WebDriverFrameWork/src/unitTests/TestDataProviderTests.java
+++ WebDriverFrameWork/src/unitTests/TestDataProviderTests.java
@@ -27,7 +27,7 @@ public class TestDataProviderTests {
 		}
 	}
 	
-	@Test
+	@Test(priority = 0)
 	public void testUsernaame() {
 		
 		String userName = testDataReader.getUserName("Test001");
@@ -35,17 +35,49 @@ public class TestDataProviderTests {
 		
 	}
 	
-	@Test
-	public void testProductID() {
-		
-		String productID = testDataReader.getProductID("Test002");
-		Assert.assertEquals(productID, "1235", "Incorrect value of Product ID retrieved");
+	@Test(priority = 1)
+	public void testPassword() {
+		String password = testDataReader.getPassword("Test002");
+		Assert.assertEquals(password, "password2", "Incorrect value of Password retrieved");
 	}
 	
-	@Test
+	@Test(priority = 2)
+	public void testFirstName() {
+		String firstName = testDataReader.getFirstName("Test003");
+		Assert.assertEquals(firstName, "Tester_03", "Incorrect value of First Name retrieved");
+	}
+	
+	@Test(priority = 3)
+	public void testLastName() {
+		String lastName = testDataReader.getLastName("Test004");
+		Assert.assertEquals(lastName, "Case_04", "Incorrect value of Last Name retrieved");
+	}
+	
+	@Test(priority = 4)
 	public void testEmail() {
 		
-		String email = testDataReader.getEmail("Test004");
-		Assert.assertEquals(email, "parinit4@abc.com", "Incorrect value of email retrieved");
+		String email = testDataReader.getEmail("Test001");
+		Assert.assertEquals(email, "parinit1@abc.com", "Incorrect value of email retrieved");
+	}
+	
+	@Test(priority = 5)
+	public void testProductName() {
+		
+		String productName = testDataReader.getProductName("Test002");
+		Assert.assertEquals(productName, "Laptop Dell", "Incorrect value of Product Name retrieved");
+	}
+	
+	@Test(priority = 6)
+	public void testProductID() {
+		
+		String productID = testDataReader.getProductID("Test003");
+		Assert.assertEquals(productID, "1236", "Incorrect value of Product ID retrieved");
+	}
+	
+	@Test(priority = 7)
+	public void testPrice() {
+		
+		String price = testDataReader.getPrice("Test004");
+		Assert.assertEquals(price, "47", "Incorrect value of Price retrieved");
 	}
 }
